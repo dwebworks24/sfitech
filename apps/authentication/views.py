3c9e2f9bdf17fc25da60b117569be7a3d879fe33
website-krysta <website.krysta@gmail.com>
2024-06-21 09:46:29 +0530

    add layout design

---
+++ apps/authentication/views.py
@@ -0,0 +1,31 @@
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, SignUpForm
+from apps.home.models import *
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == "POST":
+        if form.is_valid():
+            print("start")
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            print("end")
+            if user is not None:
+                print('use in')
+                login_user = login(request, user)
+                print(login_user)
+                return redirect("/")
+            else:
+                msg = 'Invalid credentials'
+        else:
+            msg = 'Error validating the form'
+
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
